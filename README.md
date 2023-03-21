4f006d2de16c431f2cc8aaf9624dbb794010c3d6
Menshikov.SI <89089118626@mail.ru>
2023-03-21 10:55:38 +0500

    add readme and yml

---
+++ README.md
@@ -0,0 +1,3 @@
+Домашнее задание к занятию «2.4. BDD»
+
+[![Build status](https://ci.appveyor.com/api/projects/status/vfgd4y53fayxbn2x?svg=true)](https://ci.appveyor.com/project/SeniorTe/pageobject)
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPageV1;
@@ -9,6 +11,11 @@ import ru.netology.web.page.LoginPageV3;
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
+
+  @BeforeEach
+  void setup() {
+    Configuration.holdBrowserOpen = true;
+  }
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
       open("http://localhost:9999");
